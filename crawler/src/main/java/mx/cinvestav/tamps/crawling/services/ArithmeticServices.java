75b3e829e011e5d2a5c53b811d5313fffd21134f
EALDANA <EALDANA@DRIVANLOPEZ-PC>
2017-02-24 19:27:46 -0600

    Version 24 02 2017 (ealdana)

--- crawler/src/main/java/mx/cinvestav/tamps/crawling/services/ArithmeticServices.java
+++ crawler/src/main/java/mx/cinvestav/tamps/crawling/services/ArithmeticServices.java
@@ -4,26 +4,14 @@
  * and open the template in the editor.
  */
 package mx.cinvestav.tamps.crawling.services;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
+
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.xml.bind.JAXBElement;
-import javax.xml.namespace.QName;
 
- 
 /**
  *Implements several test services.
  * @author ealdana@tamps.cinvestav.mx
@@ -31,5 +19,18 @@ import javax.xml.namespace.QName;
 @Path("math")
 public class ArithmeticServices {
     
+    @GET
+    @Path("multiplicacion")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response multiplica(@QueryParam("val1") String val1,
+                               @QueryParam("val2") String val2) {
+        float fVal1=Float.parseFloat(val1);
+        float fVal2=Float.parseFloat(val2);
+        float res=fVal1*fVal2;
+        return Response.status(200)
+                       .entity("<H1>"+fVal1+"*"+fVal2+"="+res+"</H1>")
+                       .type("text/html; charset=utf-8")
+                       .build();
+    }
     
 }
